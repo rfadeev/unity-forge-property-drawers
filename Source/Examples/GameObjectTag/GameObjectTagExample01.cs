eaa3d8addf1ef43e45fecea02a849c5a071328a9
Roman Fadeev <roma.fadeev@gmail.com>
2017-12-10 20:57:41 +0800

    Implement game object tag custom property drawer

---
+++ Source/Examples/GameObjectTag/GameObjectTagExample01.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityForge
+{
+    public class GameObjectTagExample01 : MonoBehaviour
+    {
+        [SerializeField, GameObjectTag]
+        private string exampleTag;
+
+        private void Awake()
+        {
+            Debug.LogFormat("Example tag: {0}", exampleTag);
+        }
+    }
+}
---
+++ Source/PropertyDrawers/Editor/Drawers/GameObjectTagDrawer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityForge;
+
+[CustomPropertyDrawer(typeof(GameObjectTag))]
+public class GameObjectTagDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property.propertyType != SerializedPropertyType.String)
+        {
+            position = EditorGUI.PrefixLabel(position, label);
+            EditorGUI.LabelField(position, String.Format("Error: {0} attribute can be applied only to {1} type", typeof(GameObjectTag), SerializedPropertyType.String));
+            return;
+        }
+
+        property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+    }
+}
