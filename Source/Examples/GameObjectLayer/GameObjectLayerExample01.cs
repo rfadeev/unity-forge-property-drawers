89c1961bc68ac7b793496d8ab0d06a5a86146199
Roman Fadeev <roma.fadeev@gmail.com>
2017-12-10 21:07:39 +0800

    Implement game object layer custom property drawer

---
+++ Source/Examples/GameObjectLayer/GameObjectLayerExample01.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityForge
+{
+    public class GameObjectLayerExample01 : MonoBehaviour
+    {
+        [SerializeField, GameObjectLayer]
+        private int exampleLayer;
+
+        private void Awake()
+        {
+            Debug.LogFormat("Example layer: {0}", exampleLayer);
+        }
+    }
+}
