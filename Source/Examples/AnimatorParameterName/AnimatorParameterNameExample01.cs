8224be0d374168c88c5d2f9b9e1942051a6c4b82
Roman Fadeev <roma.fadeev@gmail.com>
2017-12-13 22:49:55 +0800

    Implement animator parameter name custom property drawer

---
+++ Source/Examples/AnimatorParameterName/AnimatorParameterNameExample01.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace UnityForge
+{
+    [RequireComponent(typeof(Animator))]
+    public class AnimatorParameterNameExample01 : MonoBehaviour
+    {
+        [SerializeField, AnimatorParameterName(AnimatorControllerParameterType.Float)]
+        private string exampleFloatParameter;
+
+        [SerializeField, AnimatorParameterName(AnimatorControllerParameterType.Int)]
+        private string exampleIntParameter;
+
+        [SerializeField, AnimatorParameterName(AnimatorControllerParameterType.Bool)]
+        private string exampleBoolParameter;
+
+        [SerializeField, AnimatorParameterName(AnimatorControllerParameterType.Trigger)]
+        private string exampleTriggerParameter;
+
+        private void Start()
+        {
+            var animator = GetComponent<Animator>();
+            Debug.LogFormat("Example float parameter value: {0}", animator.GetFloat(exampleFloatParameter));
+            Debug.LogFormat("Example int parameter value: {0}", animator.GetInteger(exampleIntParameter));
+            Debug.LogFormat("Example bool parameter value: {0}", animator.GetBool(exampleBoolParameter));
+            animator.SetTrigger(exampleTriggerParameter);
+        }
+    }
+}
---
+++ Source/Examples/AnimatorParameterName/AnimatorParameterNameExample02.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace UnityForge
+{
+    public class AnimatorParameterNameExample02 : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator exampleAnimator;
+
+        [SerializeField]
+        [AnimatorParameterName(AnimatorControllerParameterType.Float, animatorField: "exampleAnimator")]
+        private string exampleFloatParameter;
+
+        [SerializeField]
+        [AnimatorParameterName(AnimatorControllerParameterType.Int, animatorField: "exampleAnimator")]
+        private string exampleIntParameter;
+
+        [SerializeField]
+        [AnimatorParameterName(AnimatorControllerParameterType.Bool, animatorField: "exampleAnimator")]
+        private string exampleBoolParameter;
+
+        [SerializeField]
+        [AnimatorParameterName(AnimatorControllerParameterType.Trigger, animatorField: "exampleAnimator")]
+        private string exampleTriggerParameter;
+
+        private void Start()
+        {
+            if (exampleAnimator != null)
+            {
+                Debug.LogFormat("Example float parameter value: {0}", exampleAnimator.GetFloat(exampleFloatParameter));
+                Debug.LogFormat("Example int parameter value: {0}", exampleAnimator.GetInteger(exampleIntParameter));
+                Debug.LogFormat("Example bool parameter value: {0}", exampleAnimator.GetBool(exampleBoolParameter));
+                exampleAnimator.SetTrigger(exampleTriggerParameter);
+            }
+        }
+    }
+}
