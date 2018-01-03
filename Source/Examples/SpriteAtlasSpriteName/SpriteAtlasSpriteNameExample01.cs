80da15891a0fc187b36f329607f2e542a5c5e853
Roman Fadeev <roma.fadeev@gmail.com>
2018-01-03 23:47:47 +0800

    Implement sprite atlas sprite name property drawer

---
+++ Source/Examples/SpriteAtlasSpriteName/SpriteAtlasSpriteNameExample01.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_2017_1_OR_NEWER
+
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace UnityForge
+{
+    public class SpriteAtlasSpriteNameExample01 : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteAtlas atlas;
+        [SerializeField, SpriteAtlasSpriteName(spriteAtlasField: "atlas")]
+        private string spriteName;
+        [SerializeField]
+        private SpriteRenderer spriteRenderer;
+
+        private void Update()
+        {
+            spriteRenderer.sprite = atlas.GetSprite(spriteName);
+        }
+    }
+}
+
+#endif
